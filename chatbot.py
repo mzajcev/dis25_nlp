7bebad238911e0222d84798e6e8a940af697c41c
matthias <80273026+mzajcev@users.noreply.github.com>
2023-07-20 19:20:19 +0200

    first try

---
+++ chatbot.py
@@ -0,0 +1,2 @@
+from classifiers.logistic_regression.logistic import load_data, preprocess_data, model_training
+from classifiers.naive_bayes.naive import load_data, prepare_data, transformations, vectorize_data
